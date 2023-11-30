186d9dc498d88e3fd8691d3897db8e113c5eaf8d
Your Name <sanazahid408@gmail.com>
2023-12-01 01:35:02 +0500

    first commit

--- sanity1/sanity.config.ts
+++ sanity1/sanity.config.ts
@@ -1,14 +1,14 @@
-import {defineConfig} from 'sanity'
-import {deskTool} from 'sanity/desk'
-import {visionTool} from '@sanity/vision'
-import {schemaTypes} from './schemas'
+import {defineConfig} from "sanity"
+import {deskTool} from "sanity/desk"
+import {visionTool} from "@sanity/vision"
+import {schemaTypes} from "./schemas"
 
 export default defineConfig({
-  name: 'default',
-  title: 'ecommerce website',
+  name: "default",
+  title: "ecommerce website",
 
-  projectId: 'r9qnqvg6',
-  dataset: 'production',
+  projectId: "r9qnqvg6",
+  dataset: "production",
 
   plugins: [deskTool(), visionTool()],
 
